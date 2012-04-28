e87576b79c7d34c5682a31b32af86c735c19e661
Hee Jung Ryu <hr99@cornell.edu>
2012-04-28 15:39:51 -0400

    Categorize the given questions in the question file
    into 6 different categories:
      what where when how who misc.
    
    It also has a function called "stats" that analyze the given questions
    to see how much percentage belongs to which question type.

---
+++ baseline/categorize.py
@@ -0,0 +1,74 @@
+import re
+"""
+  Categorize Questions into 6 different question types:
+      what, who, where, when, how, misc(miscellaneous)
+"""
+class CategorizeQs:
+
+    def __init__(self):
+        self.categories = {"what":1, "who":2, "where":3, "when":4, "how":5, "misc":6}
+
+    def categorize_q(self, question):
+        words = re.compile("\W").split(question.lower())
+        firstword = words[0]
+        if self.categories.has_key(firstword):
+            return firstword
+        return "misc"
+
+    def read_qfile(self, qfile):
+        doc = open(qfile, 'r').read()
+        m_num_qs = re.compile('(<num> Number: (\d*)|<desc> Description:\s*\n(.*))')
+        num_qs = m_num_qs.findall(doc)
+        questions = {}
+        for tup in num_qs:
+            if "<num>" in tup[0]:
+                qnum = tup[1].strip()
+            else:
+                question = tup[2].strip()
+                questions[qnum] = question
+        return questions
+
+    """
+        Extracts question numbers and questions from qfile, and 
+        categorizes each question into 6 different question types,
+        then calls the appropirate functions to answer each question
+
+        params
+        ----
+            qfile : question file in the format:
+                 <num> Number: "qnum"
+                 ...
+                 <desc> Descriptions:
+                 "question"
+                 ...
+
+    """
+    def run_catqs(self, qfile):
+        questions = self.read_qfile(qfile)
+        for (qnum,q) in questions.items():
+            print self.categorize_q(q)
+
+    def stats(self, qfile):
+        questions = self.read_qfile(qfile)
+        qstats = {}
+        tot = 0.
+        for (qnum,q) in questions.items():
+            tot += 1.
+            qtype = self.categorize_q(q)
+            if qstats.has_key(qtype):
+                qstats[qtype] += 1.
+            else:
+                qstats[qtype] = 1.
+
+        qstat_str = ""
+        for (qt,freq) in qstats.items():
+            qstats[qt] = freq / tot
+            qstat_str = qstat_str + qt + " " + str(qstats[qt]) + "%\n"
+
+        print qstat_str
+
+if __name__ == "__main__":
+    qatree = CategorizeQs()
+    qatree.stats("/Users/jollifun/Downloads/train/questions.txt")
+
+        
